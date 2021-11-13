2c69870d5d0cd5bc36730fa229ed8a786b226445
Ragnar Stefánsson <ragnarstefanss@gmail.com>
2021-11-13 20:12:27 +0000

    python file callable from ipynb

--- MovieCustomerInformation.py
+++ MovieCustomerInformation.py
@@ -18,6 +18,66 @@ from operator import itemgetter
 import NetflixLoadData as NetflixLoadData
 #sns.set_style("darkgrid")
 
+def all_id_rows(df, type, item_id):
+    return df[df[type] == item_id]
+
+def all_average_ratings(df, type='movie_id'):
+    ratings_stats = df.groupby(type).agg({'rating': ['sum', 'count']}).reset_index()
+    ratings_stats['avg_rating'] =  ratings_stats['rating']['sum'] / ratings_stats['rating']['count']
+    return ratings_stats
+
+def all_get_rated_count(df, type):
+    return df.groupby(type).agg({'movie_id': 'count'}).reset_index()
+
+def get_avg_rating_less_than(df, max_rating):
+    print(df[df['avg_rating'] < max_rating])
+
+
+def get_avg_rating_higher_than(df, min_rating):
+    print(df[df['avg_rating'] > min_rating])
+
+def get_item_avg_rating(df, type, id):
+    return df[df[type] == id]
+
+
+def get_movies_customer_rated_higher_than(df, customer_id, min_rating=4): 
+    #df is equal to data_rating_plus_movie_title
+    temp = df.copy()
+    return temp[(temp['customer_id'] == customer_id) & (temp['rating'] >= min_rating)].set_index('movie_id')
+
+def get_movies_customer_rated_lower_than(df, customer_id, max_rating=4):
+    #df is equal to data_rating_plus_movie_title
+    temp = df.copy()
+    return temp[(temp['customer_id'] == customer_id) & (temp['rating'] < max_rating)].set_index('movie_id')
+
+
+def display_movies_customer_rated_higher_than(df, customer_id, min_rating=4):
+    #df is equal to data_rating_plus_movie_title
+    df_customer_liked = get_movies_customer_rated_higher_than(df=df, customer_id=customer_id, min_rating=min_rating)
+    print(df_customer_liked[['movie_title', 'rating']])
+    all_customers_average_ratings =  all_average_ratings(df=df, type='customer_id')
+    print('average rating', get_item_avg_rating(df=all_customers_average_ratings, type='customer_id', id=customer_id))
+
+def display_movies_customer_rated_lower_than(df, customer_id, max_rating=4):
+    #df is equal to data_rating_plus_movie_title
+    df_customer_disliked = get_movies_customer_rated_lower_than(df, customer_id=customer_id, max_rating=max_rating)
+    print(df_customer_disliked[['movie_title', 'rating']])
+    all_customers_average_ratings =  all_average_ratings(df=df, type='customer_id')
+    print('average rating', get_item_avg_rating(df=all_customers_average_ratings, type='customer_id', id=customer_id))
+
+
+def get_users_loved_hated_movies(df, customer_id, minmax_rating):
+    users_ratings_higher_than_four = get_movies_customer_rated_higher_than(df, customer_id=customer_id, min_rating=minmax_rating)
+    users_ratings_lower_than_four = get_movies_customer_rated_lower_than(df, customer_id=customer_id, max_rating=minmax_rating)
+    print("User", customer_id ,"loved these movies")
+    for rating in users_ratings_higher_than_four['movie_title']:
+        print(rating)
+    print('')
+    print("and disliked these movies")
+    for rating in users_ratings_lower_than_four['movie_title']:
+        print(rating)
+
+''''
 ## id rows
 def all_id_rows(df, type, item_id):
     return df[df[type] == item_id]
@@ -140,3 +200,4 @@ def get_users_loved_hated_movies(df, customer_id, minmax_rating):
     print("and disliked these movies")
     for rating in users_ratings_lower_than_four['movie_title']:
         print(rating)
+'''
